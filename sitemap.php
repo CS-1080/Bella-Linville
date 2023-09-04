04bf403c8195c80abdb1dadc2168405c7181a08e
isabellalinville <143417137+isabellalinville@users.noreply.github.com>
2023-09-04 10:27:51 -0400

    Add files via upload
    Lab 1: initial setup

---
+++ sitemap.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CS 1080 LABS </title>
+    <meta name="author" content= "Bella Linville">
+    <meta name="description" content= "A folder of my labs from CS1080, a great introduction to the design of the World Wide Web">
+<style>
+    figure {
+        float: right;
+        text-align: left;
+        width: 30%;
+    }
+
+    img {
+        max-width:100%;
+    }
+    </style>
+    </head>
+
+    <body>
+        <figure>
+            <img alt="Thats me!" src="Images/Bella-Linville.png">
+            <figcaption>This is a photo of my on a girls trip with my mom in Savannah, Georgia </figcaption>
+
+        </figure>
+        <h1><strong>CS 1080 Lab 1</strong>Fall 2023, Section A</h1>
+        <h2><em>Bella Linville-Site Map</em>
+        <p><a href="ADMIN/code-viewer.php"> My Admin Page</a></p> 
+
+           <!--     ###########################################  -->
+    <section class="lab-layout">
+            <h2 class="header>Lab One - Setting up your own site map. </h2>
+            <section class="public-files>
+                <h3>Public Files</h3>
+                <p><a href="sitemap.php">Site map</a></p> 
+                <p><a href="sample.php">Sample Page</a></p>
+    </section>
+
+    <section class="supporting-files">
+        <h3>Supporting files</h3>
+        <p></p>
+    </section>
+
+    <section class="grader notes">
+    <h3>Notes to grader</h3>
+    <p></p>
+    </section>
+</section>
+ <!--     ###########################################  -->
+</body>
+
+</html>
+
+
+
+
+
+
+
+
