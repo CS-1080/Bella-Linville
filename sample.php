d48f5d537960ccccf28ab858dbcc96f96436351f
isabellalinville <143417137+isabellalinville@users.noreply.github.com>
2023-09-04 10:47:25 -0400

    Add files via upload
    Lab 1: adding a sample.php page

---
+++ sample.php
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<body>
+    <h1><strong>Lab 1</strong></h1>
+    <p>In this lab we created our portfolio to show off all we've learned, which will be built upon as we continue the class.</p>
