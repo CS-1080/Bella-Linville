5c7347b2a26d863df6aba8347e07d6500411a0a1
isabellalinville <143417137+isabellalinville@users.noreply.github.com>
2023-09-17 19:40:52 -0400

    Lab 3: added new about page

---
+++ about.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CS1080 Labs: Bella Linville</title>
+    <meta name="author" content= "Bella Linville">
+    <meta name="description" content= "A portfolio of all of my hard work in CS1080, my introduction to coding and the WWW">
+    <link rel="stylesheet" href="css/custom.css?version=?php print time(); ?>" type="text/css">
+<style>
+    figure {
+        float: right;
+        text align: right;
+        width: 50%;
+    }
+
+    img {
+        max-width: 100%;
+    }
+    </style>
+    </head>
+    <!--this is my comment woohooooooo yayyyyyyy-->
+
+    <body>
+        <header>
+        <h1>Alternatives to Grass Lawns</h1>
+</header>
+<nav> 
+    <a href="index.php">Home</a>
+    <a href="detail.php">Detail</a>
+    <a href="form.php">Form</a>
+</nav>
+        <figure>
+        <img alt="Moss Lawn" src="mosslawn.png">
+        <figcaption>This is a moss lawn, the first alternative touched on in this page.</figcaption>
+</figure>
+<main>
+    <section>
+        <h2>Moss Lawns</h2>
+        <p>Despite being uncommon, moss lawns can grow everywhere except arid, drought prone areas. While they are a large initial investment, it'll soon be paid off as moss lawns require little to no upkeep. These lawns are desne stands of moss, with a similar feeling to soft, grass lawns. Due to its nonvascular nature, moss never grows above roughly 4 inches. 
+        They can grow in any kind of soil, no matter the quality. It does well in difficult conditions such as slopes, or hillsides. Moss lawns are also a great way to save water, as they need very little once established.</p>
+</section>
+<section>
+    <h2>Clover Lawns</h2>
+    <p>Clover lawns also require little upkeep. A clover yard does require occasional mowing, mowing's only needed once or twice a year. Clover also requires little to no watering!! Lastly, clover lawns are naturally fertilizing. Clover is a legume, which means that it naturally fixes nitrogen, the chemical found in fertilizer which stimulates growth!</p>
+</section>
+
+<section>
+    <h2>Flower and Shrub Beds</h2>
+    <p>While flower and shrub beds do require more upkeep, these provide an amazing habitats for pollinators and other wildlife. Not to mention, it's been shown that the installation of flower bed lawns can increase property value and increase mental health.
+        What better way to increase seratonin than with pretty flowers?!</p> 
+
+</section>
+</main>
+<footer>
+    <p></a href="../index.php">Site map (Main Index)</a></p>
+</footer>
+</main>
+</html>
+
+
+
+
+
+
