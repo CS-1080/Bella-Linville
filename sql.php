43be055f4fe6b96aff44a72598e32f5782a80058
isabellalinville <143417137+isabellalinville@users.noreply.github.com>
2023-11-13 10:04:05 -0500

    Lab 9: sql to create table

--- sql.php
+++ sql.php
@@ -18,7 +18,35 @@ include 'top.php';
   ('David Holmgren', 'wrote "Permaculture: Principals and Pathways Beyond Sustainibility"', 'Another ecologist from Australia and the co-pioneer of permaculture. Holmgrens work entails laying out a system for us to live and thrive within natures means.')
   ('Toby Hemmingway', 'wrote "Gias Garden"', 'Wrote byfar the most popular book regarding permaculture and sustainable lawn care. Hemmingway utilizes ecology, biology, soil sciences, and chemistry to best help the reader understand the micro-ecosystem within their lawns')
   ('Masanobu Fukuoka', 'wrote "The One Straw Revolution"', 'Famous for his quote "if we throw mother nature out the door she comes back with a pitchfork". While Fukuokas primary focus in permaculture lies within sustainable agriculture the principles he has decidated his lifes work to can easily be transferred to sustainable lawn care.')
+  <?php
+$sql = 'SELECT fldPioneers, fldPublishments, fldImpacts from tblBigNames';
+$statement = $pdo->prepare($sql);
+$statement->execute();
+
+$records = $statement->fetchAll();
+
+foreach($records as $record){
+    print '<tr>';
+    print '<td>' . $record['fldPioneers'] . '</td>';
+    print '<td>' . $record['fldPublishments'] . '</td>';
+    print '<td>' . $record['fldImpacts'] . '</td>';
+    print '</tr>' . PHP_EOL;
+    }
+    ?>
         </pre>
+<pre>
+  CREATE TABLE tblAltLawnSurvey(
+    pmkAltLawnSurvey int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fldFirstName VARCHAR(30) DEFAULT NULL,
+    fldLastName VARCHAR(30) DEFAULT NULL,
+    fldEMail VARCHAR(50) DEFAULT NULL,
+    fldInvest TINYINT(1) DEFAULT 0,
+    fldAesthetic TINYINT (1) DEFAULT 0,
+    fldInfo TINYINT (1) DEFAULT 0,
+    fldConcern VARCHAR(15) DEFAULT 0,
+
+    
+  )
         </main>
         <?php
         include 'footer.php';
