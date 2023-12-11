6facbe164c5ad35463b0759e9b4e96e0f61a460a
isabellalinville <143417137+isabellalinville@users.noreply.github.com>
2023-12-10 20:28:04 -0500

    Final Project: edited detail page

--- detail.php
+++ detail.php
@@ -1,46 +1,40 @@
 <?php
-include 'top.php';
+include "top.php";
+
+$sql = 'SELECT pmkPhotoID, fldPhotoDate, fldName, fldImgName, fldCategory, fldPhotoDescription ';
+$sql .= 'FROM tblLandscapes WHERE fldCategory LIKE "Landscape"';
+
+$photos = '';
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$photos = $statement->fetchAll();
 ?>
-<div id="container">
-        <figure>
-        <img class="rounded" alt="A traditional lawn" src="images/ickylawn.png">
-        <figcaption>A photo of a traditional lawn, which wastes resources, contaminates groundwater and local bodies of water, and depletes local biodiversity.</figcaption>
-</figure>
-<main>
-    <div>
-    <section>
-        <h2 class="border">Increased Biodiversity</h2>
-        <p>Green spaces in urban areas can provide important habitats and feeding areas for pollinators and wildlife. In order for ecosystems to be healthiest, not only do they require an ample number of pollinators and othther wildlife. However, they not only need numbers but diversity.
-            For example, while a garden with a large amount of bees is great, if they're all one species that microecosystem isn't resilient. If a disease that affects a particular species of bee plagues the garden, if that garden only contains two species of bees half of its bee population would be desimated. Higher levels of biodiversity within an ecosystem
-            ensure its success and longevity. Most importantly, it's been observed that green spaces with higher diversity of pollinators also have higher diversity of plants. </p>
-    <p><a class="link" href="https://www.snexplores.org/article/diverse-plantlife-lawns-yards-environmental-benefits#:~:text=Yards%20planted%20with%20more%20diverse,benefit%20people%20and%20wildlife%20alike.">Sources</a></p>
-</section>
-</div>
-<div>
-<section>
-    <h2 class="border">Decreased Chemical Pollution</h2>
-    <p>In order to maintain a bright green, weed free lawn, fertilizers and herbicides are required. The chemical ingredients within these lawn treatments may benefit your plot of land, but not your local ecosystem. When you water your lawn, or when it rains,
-        these chemicals run off of your lawn at toxic levels. These chemicals are capable or leeching into groundwater, an important source of fresh drinking water, or running into bodies of water near you. Upon reaching the nearest body of water, the nitrogen and pottasium commonly found in these treatments promotes weed and algae growth.
-        These algal blooms lead to an event called eutrification, which can have deadly effects on the wildlife inhabiting that ecosystem. Fish directly affected by this process, as these cycles of algal blooms and die offs depletes the water of oxygen, essentially suffocating them. The rest of that ecosystems food chain is then indirectly affected by the absence of fish, creating a trophic cascade which affects all beings in that body of water. 
-        Ditching grass lawns, and therefore the need for treatments, alleviates this additional stress put on your local ecosystem. Trust me, the fish will thank you!</p>
-        <p><a class="link" href="https://cfpub.epa.gov/npstbx/files/marc_lawnchemicals.pdf">Sources</a></p>
-</section>
-</div>
-<div>
-<section>
-    <h2 class="border">Drought Resistance</h2>
-    <p>Seeing that 50% of urban water use is spent outdoors, switching from grass to native, drought resistant plants could cut back on water use signficantly. 
-        Year after year, resivoirs and aquifers have increasingly more stress put on them. As our global climate continues to warm, access to fresh water has become increasingly scarce. Conserving water as much as possible will help sustain your local community in the event of droughts.
-        As the demand for water rgows with a growing population, the supply is beginning to run out. Ensuring you're using your water smartly not only benefits your local community, but your local ecosystem as well seeing that water is essential for <strong>ALL</strong> organisms.</p>
-</section>
-</div>
-</div>
-</main>
-</main>
+
+   <h2 class = "photopageheadings">Calebs Landscape Photos</h2>
+   <p class = "cantread">Click on each photo for more information!</p>
+   <div class = "photo-container">
+   <?php
+
+
+if (is_array($photos)) {
+    foreach ($photos as $photo) {
+        print '<div class="photo-item">';
+        print '<a href="displayPhoto.php?aid=' . $photo['pmkPhotoID'] . '">';
+        print '<figure class="photo-container">';
+        print '<img alt="' . $photo['fldName'] . '" src="images/' . $photo['fldImgName'] . '">';
+        print '<figcaption>' . $photo['fldName'] . '</figcaption>';
+        print '</figure>';
+        print '</a>';
+        print '</div>';
+    }
+}
+?>
+
 <?php
 include 'footer.php';
 ?>
 </html>
+
 
 
 
