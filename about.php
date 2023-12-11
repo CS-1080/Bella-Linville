9c5bdc3cfbcf12ca045f92afcaed131e83e76004
isabellalinville <143417137+isabellalinville@users.noreply.github.com>
2023-12-10 20:28:52 -0500

    Final Project: worked on about page

--- about.php
+++ about.php
@@ -1,38 +1,35 @@
 <?php
 include "top.php";
-?>
-<div id="container">
-    <div>
-        <figure>
-        <img class="rounded" alt="Moss Lawn" src="images/mosslawn.png">
-        <figcaption>This is a moss lawn, the first alternative touched on in this page.</figcaption>
-</figure>
-</div>
-<main>
-    <div>
-    <section>
-        <h2 class="border">Moss Lawns</h2>
-        <p>Despite being uncommon, moss lawns can grow everywhere except arid, drought prone areas. While they are a large initial investment, it'll soon be paid off as moss lawns require little to no upkeep. These lawns are desne stands of moss, with a similar feeling to soft, grass lawns. Due to its nonvascular nature, moss never grows above roughly 4 inches. 
-        They can grow in any kind of soil, no matter the quality. It does well in difficult conditions such as slopes, or hillsides. Moss lawns are also a great way to save water, as they need very little once established.</p>
-</section>
-</div>
-<div>
-<section>
-    <h2 class="border">Clover Lawns</h2>
-    <p>Clover lawns also require little upkeep. A clover yard does require occasional mowing, mowing's only needed once or twice a year. Clover also requires little to no watering!! Lastly, clover lawns are naturally fertilizing. Clover is a legume, which means that it naturally fixes nitrogen, the chemical found in fertilizer which stimulates growth!</p>
-</section>
-</div>
-<div>
-<section>
-    <h2 class="border">Flower and Shrub Beds</h2>
-    <p>While flower and shrub beds do require more upkeep, these provide an amazing habitats for pollinators and other wildlife. Not to mention, it's been shown that the installation of flower bed lawns can increase property value and increase mental health.
-        What better way to increase seratonin than with pretty flowers?!</p> 
 
-</section>
-</div>
-</div>
-</main>
-</main>
+$sql = 'SELECT pmkPhotoID, fldPhotoDate, fldName, fldImgName, fldCategory, fldPhotoDescription ';
+$sql .= 'FROM tblLandscapes WHERE fldCategory LIKE "Portrait"';
+
+$photos = '';
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$photos = $statement->fetchAll();
+?>
+
+   <h2 class = "photopageheadings">Calebs Landscape Photos</h2>
+   <p class = "cantread">Click on each photo for more information!</p>
+   <div class = "photo-container">
+   <?php
+
+
+if (is_array($photos)) {
+    foreach ($photos as $photo) {
+        print '<div class="photo-item">';
+        print '<a href="displayPhoto.php?aid=' . $photo['pmkPhotoID'] . '">';
+        print '<figure class="photo-container">';
+        print '<img alt="' . $photo['fldName'] . '" src="images/' . $photo['fldImgName'] . '">';
+        print '<figcaption>' . $photo['fldName'] . '</figcaption>';
+        print '</figure>';
+        print '</a>';
+        print '</div>';
+    }
+}
+?>
+
 <?php
 include 'footer.php';
 ?>
